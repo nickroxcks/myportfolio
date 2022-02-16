3e8c326bd81545da46ab9be736ac24c7c75d7607
Nicholas <nicholas.serrano7@gmail.com>
2022-02-15 21:59:20 -0700

    homepage has general layout built

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,21 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
+import PropTypes from 'prop-types'
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+        <SectionTitle main center>
+            Hi, <br />
+            I'm Nicholas Serrano
+        </SectionTitle>
+        <SectionText>
+          "The only way to do great work, is to love what you do"
+        </SectionText>
+        <Button onclick={() => window.location = "https://google.com"}>Learn More</Button>
+    </LeftSection>
+  </Section> 
 );
 
 export default Hero;
